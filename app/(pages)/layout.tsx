e6fdcddcb86cb4e4e7f189367e9b431a55212967
Isaac Dzikum <nisaacdz@gmail.com>
2025-02-23 12:15:20 +0000

    Format |npm run format

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
-import { NuqsAdapter } from 'nuqs/adapters/next/app'
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 export default function RootLayout({
   children,
@@ -10,11 +10,11 @@ export default function RootLayout({
 }>) {
   const queryClient = new QueryClient();
   return (
-      <QueryClientProvider client={queryClient}>
-        <ToastContainer />
-          <NuqsAdapter >
-            <main>{children}</main>
-          </NuqsAdapter>
-      </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ToastContainer />
+      <NuqsAdapter>
+        <main>{children}</main>
+      </NuqsAdapter>
+    </QueryClientProvider>
   );
 }
