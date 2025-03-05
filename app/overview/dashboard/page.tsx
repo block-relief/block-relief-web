e93c1935234d8da1b409b73a73d905b932746de0
Isaac <nisaacdz@gmail.com>
2025-03-05 14:56:12 +0000

    Update responsiveness

--- app/overview/dashboard/page.tsx
+++ app/overview/dashboard/page.tsx
@@ -8,7 +8,7 @@ export default function DashboardPage() {
   return (
     <div className="h-full flex flex-col px-6 py-12 gap-8">
       <DonationSummary />
-      <div className="w-full h-[430px] grid md:grid-cols-2 gap-8 flex-shrink-0 flex-grow-0">
+      <div className="w-full h-auto grid xl:grid-cols-2 gap-8 flex-shrink-0 flex-grow-0 bg-fuchsia-300">
         <MonthlyStats />
         <LocationStats />
       </div>
--- components/sections/LatestCampaigns.tsx
+++ components/sections/LatestCampaigns.tsx
@@ -40,12 +40,12 @@ export default function LatestCampaigns() {
   const data = result.slice(0, 3);
 
   return (
-    <div className="flex flex-col gap-4 w-full h-[536px]">
+    <div className="flex flex-col gap-4 w-full h-auto">
       <DashboardSectionHeader
         title="Latest Campaigns"
         pageUrl="/campaigns/latest"
       />
-      <div className="w-full h-full grid grid-cols-3 gap-8">
+      <div className="w-full h-full grid lg:grid-cols-2 2xl:grid-cols-3 gap-8">
         {data.map((campaign, index) => (
           <CampaignCard key={index} campaign={campaign} />
         ))}
