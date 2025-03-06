7f10a530381237a23087c55c4ab14bd08afafe48
Isaac <nisaacdz@gmail.com>
2025-03-06 23:14:06 +0000

    Update token-based auth

--- api/auth.ts
+++ api/auth.ts
@@ -1,15 +1,27 @@
 import { LocalUser } from "@/types";
 import Api, { ApiResponse } from ".";
 
-export async function me(): Promise<ApiResponse<{ user: LocalUser | null }>> {
+// Have to make sure that this route is not protected in the backend
+// It should be accessible without a token, returning a 200 status code on every request if possible
+// Should return { user: LocalUser } if the token is valid, otherwise return { user: null }
+// This api must not automatically redirect to login by returning a 401 status code
+// Redirecting will exist for other protected routes and will be handled by the AuthContextProvider
+export async function me(): Promise<ApiResponse<LocalUser>> {
   return Api.get("/auth/me");
 }
 
 export async function login(logins: {
   email: string;
   password: string;
-}): Promise<ApiResponse<LocalUser>> {
-  return Api.post("/auth/login", logins);
+}): Promise<ApiResponse<{ access: string; refresh: string }>> {
+  const response = await Api.post<{ access: string; refresh: string }>(
+    "/auth/login",
+    logins,
+  );
+  if (response.result) {
+    Api.setToken(response.result);
+  }
+  return response;
 }
 
 export async function logout(): Promise<ApiResponse<string>> {
